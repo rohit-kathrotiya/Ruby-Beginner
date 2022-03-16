c2c10220bff42771f8d7507e06b8a46b6cdc9078
Rohit Kathrotiya <84105090+rohit-kathrotiya@users.noreply.github.com>
2022-03-16 19:36:17 +0100

    User input

---
+++ User_input.rb
@@ -0,0 +1,11 @@
+puts "Enter Your Name : "
+Name = gets                      # gets use for getting user input
+puts ("Hello " + Name)  
+
+puts "Enter Your Surname : "
+Surname = gets.chomp()                      # chomp is use for print output in single line
+puts ("Hello " + Name + Surname + ", How are you !")  
+
+puts "Enter Your Age : "
+Age = gets.chomp()                      # chomp is use for print output in single line
+puts ("Hello " + Name + Surname + ", you are " + Age + " years old.")  
