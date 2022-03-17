238d3a15fa14c3d1b6b688933f5d4071d72b242d
Rohit Kathrotiya <rohitkathrotiya63@gmail.com>
2022-03-17 17:34:37 +0100

    While loop

---
+++ while_loop.rb
@@ -0,0 +1,20 @@
+# While Loop
+
+# it run until the conditions become false.
+
+no = 1
+while no <= 10
+    puts no
+    no += 1
+end
+
+
+# Multiplication of numbers
+
+no = 2
+i = 1
+while i <= 10
+    puts no * i
+    i += 1
+end
+  
