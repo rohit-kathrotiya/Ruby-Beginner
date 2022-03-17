b7a9d3a30a6de378764f88e66002e45b96a9d5b8
Rohit Kathrotiya <rohitkathrotiya63@gmail.com>
2022-03-17 14:05:10 +0100

    Hashes function and defination

---
+++ Hashes.rb
@@ -0,0 +1,15 @@
+# Learn hash function
+# assign more values and its key in array
+
+State = {
+    "Hannover" => "HN",
+    "Frankfurt" => "FRA",
+    "Berlin" => "BR",
+    :Munchen => "MN",
+    5 => "5"
+}
+
+puts State
+puts State["Hannover"]
+puts State[:Munchen]
+puts State[5]
