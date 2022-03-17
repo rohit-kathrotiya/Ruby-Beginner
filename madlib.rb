2c6b97a1374215c23aaca2eb7da8a6591809d192
Rohit Kathrotiya <rohitkathrotiya63@gmail.com>
2022-03-17 13:32:18 +0100

    Simple madlib program

---
+++ madlib.rb
@@ -0,0 +1,12 @@
+# Simple try to get user input and print it.
+
+puts "Enter Colour : "
+Colour = gets.chomp()
+puts "Enter prural noun : "
+Noun = gets.chomp()
+puts "Enter Celebrity Name : "
+Celeb = gets.chomp()
+
+puts ("Roses are " + Colour)
+puts (Noun + " are blue")
+puts ("I love " + Celeb)
